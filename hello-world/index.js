bc11a6b1586770d5daa18ec04e0667bd1d15a50a
Gayathri-69 <127761746+Gayathri-69@users.noreply.github.com>
2023-05-27 18:59:37 +0530

    Delete hello-world directory

--- hello-world/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable space-before-function-paren */
-/* eslint-disable semi */
-/* eslint-disable indent */
-/* eslint-disable quotes */
-function hello () {
-    console.log("Hello node.js");
-}
-hello();
