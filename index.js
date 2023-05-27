9e2acde7e46b237ea1a2e232f41b3d2e91822d40
Gayathri-69 <127761746+Gayathri-69@users.noreply.github.com>
2023-05-27 19:01:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+function hello () {
+    console.log("Hello node.js");
+}
+hello();
