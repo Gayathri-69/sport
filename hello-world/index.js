cf074a0f8d224a841ad6b9cca7e9027bc973fdee
Gayathri <linux_gayathri@DESKTOP-669SLPA.localdomain>
2023-05-27 15:46:19 +0530

    added

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,4 +1,8 @@
-const hello=()=>{
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+function hello () {
     console.log("Hello node.js");
-};
+}
 hello();
